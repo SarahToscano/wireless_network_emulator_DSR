0f5b2827d77612ed4c22cb36a3fe08d651aed07e
Jackson De Araujo Limeira <36930468+jackineo@users.noreply.github.com>
2021-07-02 19:30:58 -0300

    Update files

--- layer_network.py
+++ layer_network.py
@@ -6,54 +6,55 @@ from header import Header
 from layer_physical import Physical_Layer
 from layer_link import Link_Layer
 
+
 class Network_layer:
 
-    def __init__(self, layer_link):
-            self._link_layer = layer_link
-            self._pcks_list = []
-            self._RREQS_list = []
-            self._wait_routes_list = []
-            self._routes = []
-    
+    def __init__(self, Link_Layer):
+        self._link_layer = Link_Layer
+        self._pcks_list = []
+        self._RREQS_list = []
+        self._wait_routes_list = []
+        self._routes = []
+
     def RREP(self, mac_final, seq, route):
-        id = self._layer_link._layer_physical._mac
+        id = self._link_layer._Physical_Layer._mac
         header = Header("Network", id, mac_final, -1, 1, -1, seq)
-        pck = Package(route, 1) #Create the package
-        pck.add_header(header) #Insert the Network Layer's header
+        pck = Package(route, 1)  # Create the package
+        pck.add_header(header)  # Insert the Network Layer's header
 
         msg = "Sending RREP"
         self.show_package_Mac(msg, id, mac_final)
 
         # Define the request route
-        for i, mac in enumerate(header.seq_list):
+        for i, mac in enumerate(header.num_pack):
             if(mac == id):
-                next_node = header.seq_list[i+1]
+                next_node = header.num_pack[i+1]
                 next_pck = pck
-                self._layer_link.add_pck(next_pck, next_node)
+                self._link_layer.add_pck(next_pck, next_node)
                 break
 
     def RREQ(self, mac_final):
-        id = self._layer_link._layer_physical._mac
+        id = self._link_layer._Physical_Layer._mac
         seq = []
-        seq.append(id) #add first ID
-        seqNum = random.randint(1, 50) #package num
+        seq.append(id)  # add first ID
+        seqNum = random.randint(1, 50)  # package num
         self._RREQS_list.append(seqNum)
 
         header = Header("Network", id, mac_final, -1, 0, seqNum, seq)
-        pck = Package("", 1) #Create the package
-        pck.add_header(header) #Insert the Network Layer's header
+        pck = Package("", 1)  # Create the package
+        pck.add_header(header)  # Insert the Network Layer's header
         msg = "Sending RREQ "
 
-        self.show_package_Mac(msg, id, None) # The dif of RREP Mac_final here is None
+        # The dif of RREP Mac_final here is None
+        self.show_package_Mac(msg, id, None)
         self._link_layer.add_pck(pck, -1)
 
     def add_pck(self, mac_final, mensage, t):
-        id = self._camadaEnlace._camadaFisica._mac
+        id = self._link_layer._Physical_Layer._mac
         pck = Package(mensage, t)
         header = Header("REDE", id, mac_final, -1, -1, -1, None)
         pck.add_header(header)
-        self._pcks_list.append(pck) #Add package to list
-
+        self._pcks_list.append(pck)  # Add package to list
 
     def show_package(self, mensagem, id, mac_final):
         print("ID: ", id, "Mensg: ", mensagem, " macDest: ", mac_final)
@@ -66,7 +67,7 @@ class Network_layer:
               " Numero de seq: ", sequenNum)
 
     def send_pack(self):
-        if(self._pcks_list != []): #is there any package?
+        if(self._pcks_list != []):  # is there any package?
             pckg = self._pcks_list[0]
             header = pckg.get_header_network()
             seq = None
@@ -84,12 +85,12 @@ class Network_layer:
                 self._pcks_list.pop(0)
 
                 for i, mac in enumerate(pckg._headers[0]._seq_list):
-                    id = self._layer_link._layer_physical._mac
+                    id = self._link_layer._Physical_Layer._mac
                     if(mac == id):
-                        next_node = header.seq_list[i+1]
+                        next_node = header.num_pack[i+1]
                         break
-                self._layer_link.add_pck(pckg, next_node)
-                id = self._layer_link._layer_physical._mac
+                self._link_layer.add_pck(pckg, next_node)
+                id = self._link_layer._Physical_Layer._mac
                 mac_id_send_list.append(id)
 
             elif(not header.final_mac in self._wait_routes_list):
@@ -99,5 +100,110 @@ class Network_layer:
                 self.RREQ(pckg._headers[0].final_mac)
 
         # Send pckgs to link layer
-        self._layer_link.send_pack()
+        self._link_layer.send_pack()
 
+    def receive_pack(self):
+        self._link_layer.recebePacote()  # Trata pacote recebido na camada de enlace
+
+        # Verifica se tem pacotes recebidos
+        if(self._link_layer._pck_read != []):
+            pckg = self._link_layer._pck_read.pop(0)
+            header = pckg.get_header_network()
+
+            # Se o pckg for recebido for de dados
+            if(header._req == -1):
+                id = self._link_layer._Physical_Layer._mac
+                # Verifica se o pckg é para aquele host
+                if(header._final_mac == id):
+                    msg = "pckg de dados: "
+                    self.show_package_Mac(
+                        msg, id, pckg._data)
+                else:
+                    msg = "Chegada de pacote de dados mas não é pra mim "
+                    self.show_package(
+                        msg, self._link_layer._Physical_Layer._mac, 0)
+                    msg_2 = "Enviando pacote de dados para o nó seguinte"
+                    self.show_package(
+                        msg_2, self._link_layer._Physical_Layer._mac, 0)
+
+                    for i, mac in enumerate(pckg._headers[0]._seq_list):
+                        id = self._link_layer._Physical_Layer._mac
+                        if(mac == id):
+                            next_node = header.num_pack[i-1]
+                            break
+
+                    pckg._headers.pop(1)
+                    self._link_layer.add_pck(pckg, next_node)
+
+                    id = self._link_layer._Physical_Layer._mac
+                    mac_id_send_list.append(id)
+
+            # Se o pacote for recebido for um RREQ
+            elif(header._req == 0):
+                mensage = "Chegada de pacote RREQ"
+                self.show_sequence_num(
+                    mensage, self._link_layer._Physical_Layer._mac, header.num_pack)
+                # Verifica se esse RREQ já foi recebido pelo nó
+                if(not header.num_pack in self._RREQS_list):
+                    self._RREQS_list.append(header.num_pack)
+                    header.seq_list.append(
+                        self._link_layer._Physical_Layer._mac)
+
+                    # Verifica se o RREQ é para o nó
+                    if(header._final_mac == self._link_layer._Physical_Layer._mac):
+                        mensage = "Eu sou o destino do RREQ"
+                        self.show_package(
+                            mensage, self._link_layer._Physical_Layer._mac, 0)
+                        route = header.seq_list
+                        final_mac = route[0]
+                        sequence_route = route
+                        sequence_route.reverse()
+                        self.send_RREP(final_mac, sequence_route, route)
+                        mac_id_send_list.append(
+                            self._link_layer._Physical_Layer._mac)
+
+                    else:
+                        print("ID: ", self._link_layer._Physical_Layer._mac,
+                              "Eu não sou o destino do RREQ")
+                        self._link_layer.add_pck(pckg, -1)
+                        mac_id_send_list.append(
+                            self._link_layer._Physical_Layer._mac)
+
+                else:
+                    mensage = "Ja tenho esse RREQ"
+                    self.show_package_Seq(
+                        mensage, self._link_layer._Physical_Layer._mac, header.num_pack)
+            # Se o pacote for recebido for um RREP
+            elif(header._req == 1):
+                dest = header._final_mac
+                mensage = "Chegada de pacote RREP: "
+                self.show_package_Seq(
+                    mensage, self._link_layer._Physical_Layer._mac, header.seq_list)
+                # Verifica se o RREP é para o nó
+                if(dest == self._link_layer._Physical_Layer._mac):
+                    mensage = "Eu sou o destino do RREP"
+                    self.show_package(
+                        mensage, self._link_layer._Physical_Layer._mac, 0)
+                    mensage_2 = "Enviando dados"
+                    self.show_package(
+                        mensage, self._link_layer._Physical_Layer._mac, 0)
+                    sequence_route = pckg._data
+                    route = Route(header.seq_list[0], sequence_route)
+                    self._routes.append(route)
+                    mac_id_send_list.append(
+                        self._link_layer._Physical_Layer._mac)
+
+                else:
+                    mensage = "Eu não sou o destino do RREP"
+                    self.show_package(
+                        mensage, self._link_layer._Physical_Layer._mac, 0)
+                    for i, mac in enumerate(header.seq_list):
+                        if(mac == self._link_layer._Physical_Layer._mac):
+                            next_host = header.seq_list[i+1]
+                            next_pck = pckg
+                            pckg._headers.pop(1)
+                            self._link_layer.add_pck(
+                                next_pck, next_host)
+                            mac_id_send_list.append(
+                                self._link_layer._Physical_Layer._mac)
+                            break
--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 from host import host
-from physical_layer import physical
+from layer_physical import Physical_Layer
 import logging
 
-logging.basicConfig(filename='report.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='report.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().setLevel(logging.DEBUG)
-logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%I:%M:%S')
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%I:%M:%S')
 
 logging.info('Starting the application...')
 
-#Defining the hosts
+# Defining the hosts
 n_hosts = 5
 logging.info(f'Creating {n_hosts} Wireless Hosts...')
-h0 = host (x=1, y=0, reach=4, mac=0)
-h1 = host (x=2, y=3, reach=4, mac=1)
-h2 = host (x=3, y=5, reach=4, mac=2)
-h3 = host (x=6, y=8, reach=4, mac=3)
-h4 = host (x=5, y=7, reach=4, mac=4)
+h0 = host(x=1, y=0, reach=4, mac=0)
+h1 = host(x=2, y=3, reach=4, mac=1)
+h2 = host(x=3, y=5, reach=4, mac=2)
+h3 = host(x=6, y=8, reach=4, mac=3)
+h4 = host(x=5, y=7, reach=4, mac=4)
 
-hosts_list = [h0, h1,h2,h3,h4]
-sender_list = [h1,h3]
+hosts_list = [h0, h1, h2, h3, h4]
+sender_list = [h1, h3]
 h1.sender(mensage=["m1", "m2", "m3"], ID=4)
 h3.sender(mensage=["m6"], ID=0)
 
-for i in range (0, len(sender_list)):
+for i in range(0, len(sender_list)):
     #req = host.requests_n(sender_list[i], hosts_list)
-    req = physical.physical_send(sender_list[i], hosts_list)
+    req = Physical_Layer.physical_send(sender_list[i], hosts_list)
 
 logging.warning(f'{req} mensage requests')
-
-
